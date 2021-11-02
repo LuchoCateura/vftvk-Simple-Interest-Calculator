80309ad06062c6888ab3be985844abb9b2ba9fe9
LuchoCateura <juniorcateura@hotmail.com>
2021-11-02 20:38:17 -0300

    modified base project to complete final project of week 6

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var p  = parseInt(document.getElementById("principal").value);
+    var ir = parseFloat(document.getElementById("rate").value);
+	var y  = parseInt(document.getElementById("years").value);
+	var ar = p*(ir/100)*y;
+	var fy = 2021+y;
+
+	document.getElementById("ResultText").innerHTML = "If you deposit " + p + ",</br> at an interest rate of " + ir + " %.</br> You will receive an amount of " + ar + ",</br>in the year of "+ fy;
 }
-        
